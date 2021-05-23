e0e75ae2c5af7e93446c8e617f4c532776f413ec
An39nA0219 <an39na10282004@yahoo.co.jp>
2021-05-24 01:12:00 +0900

    blobURL⇒BASe64デコードmigi保存

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,25 @@
+class BooksController < ApplicationController
+  # GET /books
+  def index
+    @books = Book.with_attached_book_image
+    render 'index', formats: 'json', handlers: 'jbuilder'
+  end
+
+  # POST /books
+  def create
+    @book = Book.new(book_params)
+
+    if @book.save
+      @book.parse_base64(params[:image])
+      render json: @book, status: :created, location: @book
+    else
+      render json: @book.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :image)
+  end
+end
